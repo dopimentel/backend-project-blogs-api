742463be8c76094d8b7d09e435c1f5202647118c
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-14 12:35:57 -0300

    feat: req02

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+  'User',
+  {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'display_name',
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+  return User;
+};
